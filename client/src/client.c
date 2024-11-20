f924ca7987613cd971702d2e2cf7938d17b0db27
Abi Saleh Adrian <aabisaleh@2D-LINUX-054.insa-lyon.fr>
2024-11-20 02:10:36 +0100

    control c signal handled

--- client/src/client.c
+++ client/src/client.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
+
 
 #include "client.h"
 
@@ -154,6 +156,8 @@ int main(int argc, char **argv)
       printf("Usage : %s [address] [pseudo]\n", argv[0]);
       return EXIT_FAILURE;
    }
+
+   signal(SIGINT, SIG_IGN);
 
    init();
 
